e652f944a27576c14dee35f79d291cb164d46af0
Serhii Chernikov <55809302+perseverance50k@users.noreply.github.com>
2024-06-05 10:28:29 +0300

    Implemented normalization for notifications data using entity adapter

--- src/features/notifications/notificationsSlice.js
+++ src/features/notifications/notificationsSlice.js
@@ -1,6 +1,14 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import {
+  createSlice,
+  createAsyncThunk,
+  createEntityAdapter,
+} from '@reduxjs/toolkit'
 
 import { client } from '../../api/client'
+
+const notificationsAdapter = createEntityAdapter({
+  sortComparer: (a, b) => b.date.localeCompare(a.date),
+})
 
 export const fetchNotifications = createAsyncThunk(
   'notifications/fetchNotifications',
@@ -20,21 +28,20 @@ export const fetchNotifications = createAsyncThunk(
 
 const notificationsSlice = createSlice({
   name: 'notifications',
-  initialState: [],
+  initialState: notificationsAdapter.getInitialState(),
   reducers: {
     allNotificationsRead: (state) => {
-      state.forEach((notification) => (notification.read = true))
+      Object.values(state.entities).forEach((notification) => {
+        notification.read = true
+      })
     },
   },
   extraReducers: (builder) => {
     builder.addCase(fetchNotifications.fulfilled, (state, action) => {
-      state.push(...action.payload)
-      state.forEach((notification) => {
-        // Any notifications we've read are no longer new
+      notificationsAdapter.upsertMany(state, action.payload)
+      Object.values(state.entities).forEach((notification) => {
         notification.isNew = !notification.read
       })
-      // Sort with newest first
-      state.sort((a, b) => b.date.localeCompare(a.date))
     })
   },
 })
@@ -43,4 +50,5 @@ export default notificationsSlice.reducer
 
 export const { allNotificationsRead } = notificationsSlice.actions
 
-export const selectAllNotifications = (state) => state.notifications
+export const { selectAll: selectAllNotifications } =
+  notificationsAdapter.getSelectors((state) => state.notifications)
